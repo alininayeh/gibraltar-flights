80d04a4ab502bdc0d270cbd6ed568cbdccf40de8
Alin Inayeh <>
2019-08-22 12:31:13 +0300

    UI improvements

--- src/components/Flight.component.js
+++ src/components/Flight.component.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { getStatusColor } from '../reducers';
 
 export const Flight = ({flight}) => (
-    <div className='Flight'>
+    <div className={`Flight ${getStatusColor(flight.status)}`}>
         <span className='Flight-time'>{flight.time}</span>
         <span className='Flight-place'>{flight.type === 'arrivals' ? 'Coming from' : 'Going to'} {flight.place}</span>
-        <span className={`Flight-status ${getStatusColor(flight.status)}`}>{flight.status}</span>
+        <span className='Flight-status'>{flight.status}</span>
     </div>
 );
