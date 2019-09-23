dc5d515cb8962c3307bdd5a87cc993738222d943
Alin Inayeh <>
2019-09-23 16:40:17 +0300

    Edit service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,7 @@
 self.addEventListener('install', e => {
     console.log('install service worker', e);
 });
+
+self.addEventListener('activate', e => {
+    console.log('activate service worker', e);
+});
