84b9238194c74029e2becf4abf2fa08abfc14333
Alin Inayeh <>
2019-09-23 17:23:02 +0300

    Edit service worker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/App.container';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<AppContainer />, document.getElementById('root'));
+serviceWorker.register();
